f2eb631b9a25bccd2bf1263576175bb5fc9d2fdd
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-01-04 20:50:57 +0000

    add documentation for `constexpr_while`.

--- Main.cpp
+++ Main.cpp
@@ -26,5 +26,6 @@ int main()
 	auto tuple = mlib::select(t3, std::index_sequence<1, 2>{});
 	mlib::for_each(tuple, [](auto& t) {std::cout << "val: " << t << "\n"; });
 
-	mlib::constexpr_while < 0, [&](int t) {t++; return t < 4; }, [&]() {std::cout << "Hello\n"; }, [](int t) {return t + 1; } > ();
+	// note need to add to to less than:                 --\/, will print one hello.
+	mlib::constexpr_while < 0, [&](int t) {t++; return t < 3; }, [&]() {std::cout << "Hello\n"; }, [](int t) {return t + 1; } > ();
 }
