4d34a2a3f457960cb6e96393a073080b6651d4d2
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-01-15 09:38:35 +0000

    add `get_nth_element.hpp`.

---
+++ get_nth_element.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace mlib {
+    template <auto N> constexpr auto get_nth_element(auto... args) {
+        return[&]<std::size_t... Indexes>(std::index_sequence<Indexes...>) {
+            return [](decltype((void*)Indexes)... DontCare, auto* arg,
+                auto *...DontCareEither) { return *arg; }(&args...);
+        }(std::make_index_sequence<N>{});
+    }
+} // namespace mlib
