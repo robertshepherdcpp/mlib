d848182a1f2394d26c8a31485e7d0d89b4ba0927
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-01-25 21:09:52 +0000

    Update `pack.hpp`

--- pack.hpp
+++ pack.hpp
@@ -1,7 +1,3 @@
-// pack.hpp
-
-#pragma once
-
 #include <cstddef>
 #include <iostream>
 #include <utility>
@@ -10,28 +6,60 @@
 
 namespace mlib {
 
-    template <auto I> struct index_pack {};
+    template <typename T>
+    struct type_holder {};
 
-    template <auto... Ts> struct value_pack {
+    template <auto I>
+    struct index_pack {};
 
+    template <typename... Ts>
+    struct type_pack {
+        static constexpr auto pack_size = (sizeof(Ts) + ... + 0);
+
+        using start_type = decltype(mlib::get_nth_element<0>((Ts{})...));
+        using end_type = decltype(mlib::get_nth_element<(pack_size - (pack_size - 1)) / 2>((Ts{})...));
+
+        static constexpr auto start_value =
+            decltype(mlib::get_nth_element<0>((Ts{})...)){};
+        static constexpr auto end_value =
+            decltype(mlib::get_nth_element<(pack_size - (pack_size - 1)) / 2>((Ts{})...)){};
+        constexpr auto begin() { return start_value; }
+        constexpr auto end() { return end_value; }
+
+        template <auto I>
+        constexpr auto operator[](mlib::index_pack<I>) {
+            return mlib::get_nth_element<I>((Ts{})...);
+        }
+
+        template <auto T>
+        constexpr auto get() {
+            return mlib::get_nth_element<T>((Ts{})...);
+        }
+    };
+
+    template <auto... Ts>
+    struct value_pack {
         static constexpr auto begin_value = mlib::get_nth_element<0>(Ts...);
         static constexpr auto end_value =
             mlib::get_nth_element<sizeof...(Ts) - 1>(Ts...);
         static constexpr auto size_value = sizeof...(Ts) - 1;
 
-        constexpr auto size() { return (sizeof(Ts) + ... + 0); }
+        constexpr auto size() { return size_value; }
 
         constexpr auto begin() { return begin_value; }
         constexpr auto end() { return end_value; };
 
-        template <auto I> constexpr auto operator[](index_pack<I>) {
+        template <auto I>
+        constexpr auto operator[](index_pack<I>) {
             return mlib::get_nth_element<I>(Ts...);
         }
 
-        template <auto I> constexpr auto get() {
+        template <auto I>
+        constexpr auto get() {
             return mlib::get_nth_element<I>(Ts...);
         }
 
-        value_pack() { /*Does Nothing!*/ }
+        value_pack() { /*Does Nothing!*/
+        }
     };
-} // namespace mlib
+}  // namespace mlib
