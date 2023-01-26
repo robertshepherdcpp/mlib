d0127a6b1162857d2578d38fdc3d546b458788ea
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-01-26 21:16:44 +0000

    update `fixed_string.hpp`.

--- fixed_string.hpp
+++ fixed_string.hpp
@@ -1,13 +1,17 @@
-template<auto N>
-struct fixed_string
-{
-    constexpr fixed_string(const char(&arr)[N])
-    {
-        for (int i = 0; i < N; i++)
-        {
-            m_arr[i] = arr[i];
-        }
-    }
+#include<cstddef>
 
-    char m_arr[N]{};
-};
+namespace mlib
+{
+    template<std::size_t i>
+    struct fixed_string
+    {
+        constexpr fixed_string(const char(&arr)[i + 1])
+        {
+            std::copy_n(arr, i + 1, data);
+        }
+
+        auto operator<=>(const fixed_string&) const = default;
+
+        char data[i + 1] = {};
+    };
+} // namespace mlib
