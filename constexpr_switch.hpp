445820175f67d40f55e2e07f6c7336fb9d0acf94
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-02-10 21:06:36 +0000

    add `constexpr_parameter.hpp`.

---
+++ constexpr_switch.hpp
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <iostream>
+#include <variant>
+
+namespace mlib
+{
+    template <auto T> struct constexpr_parameter {
+        using type = decltype(T);
+    };
+
+    template <auto T> struct case_ {
+        using type = decltype(T);
+    };
+
+    template <typename... Ts> struct overload : Ts... {
+        using Ts::operator()...;
+    };
+
+    template <auto callable> struct switcher {
+        constexpr auto operator()(constexpr_parameter<callable>) {
+            return callable();
+        }
+    };
+
+    template <auto... members>
+    struct switch_expressions : constexpr_parameter<members>::type... {
+        using decltype(members)::operator()...;
+    };
+
+    // a constexpr switch
+    template <auto value, auto... cases> struct constexpr_switch {
+        switch_expressions<cases...> m{};
+
+        constexpr auto operator()() { return m(case_<value>{}); }
+    };
+} // namespace mlib
