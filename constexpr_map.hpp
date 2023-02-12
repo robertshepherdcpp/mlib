75234fe900e3ddd90831857d555aeb204ede2408
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-02-12 16:46:51 +0000

    add more features to `constexpr_map`.

--- constexpr_map.hpp
+++ constexpr_map.hpp
@@ -3,57 +3,83 @@
 #include <algorithm>
 #include <concepts>
 #include <cstddef>
+#include <iostream>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
-namespace mlib
-{
-    template <auto X> struct constexpr_parameter 
-    {
+#include "get_nth_element.hpp"
+
+namespace mlib {
+
+    template <auto X> struct constexpr_parameter {
         using type = decltype(X);
     };
 
-    template<auto T>
-    static constexpr auto c_p = constexpr_parameter<T>{};
+    template <auto T> static constexpr auto c_p = constexpr_parameter<T>{};
 
     template <auto A, auto B> struct member_map {
         constexpr auto operator[](constexpr_parameter<A>) const noexcept {
             return B;
         }
+
+        constexpr auto key() { return A; }
+        constexpr auto value() { return B; }
+
+        template<auto X> constexpr auto has_key() { return X == A; }
+        template<auto X> constexpr auto has_value() { return X == B; }
     };
 
-    // a member with a variadic number of elements, not used here though.
-    template <auto... members_of> struct variadic_member
+    template<bool T>
+    constexpr auto is_not_void(constexpr_parameter<T>)
     {
-    };
+        return true;
+    }
 
-    template<auto member> struct single_member
+    template<auto T>
+    constexpr auto is_not_void(constexpr_parameter<T>)
     {
-        constexpr auto operator[](constexpr_parameter<member>) const noexcept
-        {
-            return member;
-        }
-    };
-
-    template<typename... members>
-    struct inheriter : members...
-    {
-        using members::operator[]...;
-    };
-
-    template<typename T>
-    struct constexpr_map_base_
-    {
-        using type = decltype(componentContainer<T>());
-    };
+        return false;
+    }
 
     template <auto... members>
     struct constexpr_map : constexpr_parameter<members>::type... {
         using decltype(members)::operator[]...;
 
-        template <auto T>
-        constexpr auto lookup() const noexcept {
+        template <auto T> constexpr auto lookup() const noexcept {
             return this->operator[](constexpr_parameter<T>{});
+        }
+
+        template <auto T> constexpr auto get() const noexcept {
+            return this->operator[](constexpr_parameter<T>{});
+        }
+
+        template <auto T>
+        constexpr auto has_value(constexpr_parameter<T>) noexcept {
+            return[]<std::size_t... indexes>(std::index_sequence<indexes...>) {
+                return ((mlib::get_nth_element<indexes>(members...).template has_value<T>() || ...)) || get_nth_element<sizeof...(members) - 1>(members...).template has_value<T>();
+            }(std::make_index_sequence<sizeof...(members) - 1>{});
+        }
+
+        template <auto T>
+        constexpr auto has_key(constexpr_parameter<T>) noexcept {
+            return[]<std::size_t... indexes>(std::index_sequence<indexes...>) {
+                return ((mlib::get_nth_element<indexes>(members...).template has_key<T>() || ...)) || get_nth_element<sizeof...(members) - 1>(members...).template has_key<T>();
+            }(std::make_index_sequence<sizeof...(members) - 1>{});
+        }
+
+        constexpr auto get_keys() noexcept {
+            return[]<std::size_t... indexes>(std::index_sequence<indexes...>) {
+                return std::make_tuple(
+                    (mlib::get_nth_element<indexes>(members...).key())...);
+            }(std::make_index_sequence<sizeof...(members)>{});
+        }
+
+        constexpr auto get_values() noexcept {
+            [] <std::size_t... indexes>(std::index_sequence<indexes...>) {
+                return std::make_tuple(
+                    (mlib::get_nth_element<indexes>(members...).value())...);
+            }(std::make_index_sequence<sizeof...(members)>{});
         }
     };
 } // namespace mlib
