a82b3a1a98e6c0821ce8a78fe31563e94d28061a
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-02-16 20:26:00 +0000

    add the `.at()` member function.

--- value_sequence.hpp
+++ value_sequence.hpp
@@ -1,9 +1,16 @@
 #pragma once
 
+#include"get_nth_element.hpp"
+
 namespace mlib
 {
 	template<auto... Ts>
 	struct value_sequence
 	{
+		template<auto I>
+		constexpr auto at()
+		{
+			return mlib::get_nth_element<I>(Ts...);
+		}
 	};
 } // namespace mlib
