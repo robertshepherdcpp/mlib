0ba925c02733fa4dc9a7a400c5f1f95cec9adc51
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-02-18 21:53:49 +0000

    add `value_range.hpp`.

---
+++ value_range.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstddef>
+#include <functional>
+#include <iostream>
+#include <utility>
+
+#include"get_nth_element.hpp"
+
+namespace mlib {
+    template <auto... Ts> struct value_range {
+        constexpr auto operator|(auto lambda) const { return for_each<lambda>(); }
+
+        template <auto lambda> constexpr auto for_each() const {
+            return value_range<lambda(Ts)...>{};
+        }
+
+        template <auto I> constexpr auto at() const { return std::get<I>(tuple()); }
+
+        constexpr auto tuple() const { return std::make_tuple(Ts...); }
+    };
+} // namespace mlib
