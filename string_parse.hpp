b6663470535ba8876e12db3d37668826ce6ed3c1
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-03-01 19:41:29 +0000

    add `string_parse.hpp`.

---
+++ string_parse.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <algorithm>
+#include <array>
+#include <compare>
+#include <cstddef>
+
+#include"fixed_string.hpp"
+
+// https://www.godbolt.org/z/KcWd9Wfzo
+
+namespace mlib
+{
+
+    template <fixed_string str, char c, int index>
+    constexpr auto find_index_arr() {
+        if constexpr (str.template nth_element<index>() == c) {
+            return index;
+        }
+        else {
+            return find_index_arr<str, c, index + 1>();
+        }
+    }
+
+    template <fixed_string str, char c>
+    constexpr auto find_index_arr() {
+        if constexpr (str.template nth_element<0>() == c) {
+            return 0;
+        }
+        else {
+            return find_index_arr<str, c, 1>();
+        }
+    }
+
+    template <fixed_string str> struct string_parse {
+        template <auto A, auto B> constexpr auto substr()
+        {
+            return[&]<std::size_t... indexes>(std::index_sequence<indexes...>)
+            {
+                return std::array{ (str.template nth_element<A + indexes>())... };
+            }(std::make_index_sequence<B - A>{});
+        }
+
+        template <char c> constexpr auto consume_until() {
+            constexpr int index = find_index_arr<str, c>();
+            return substr<0, index>();
+        }
+    };
+} // namespace mlib
