e6b87f916689f6f4df81fb269f2179075fb6476e
Robert Shepherd <113625774+robertshepherdcpp@users.noreply.github.com>
2023-03-09 20:50:55 +0000

    reuse already written code.

--- tuple_find.hpp
+++ tuple_find.hpp
@@ -4,34 +4,17 @@
 #include<cstddef>
 #include<tuple>
 
+#include"pack_find.hpp"
 #include"get_nth_element.hpp"
 
 namespace mlib
 {
-    template<auto Index, auto value, auto value_two>
-    struct value_is_value
-    {
-        constexpr auto operator()() const
-        {
-            if constexpr (decltype(value_two)(value) == value_two)
-            {
-                return Index;
-            }
-            else
-            {
-                return 0;
-            }
-        }
-
-        constexpr value_is_value() {}
-    };
-
     template<auto value_to_find, typename... pack>
     constexpr auto tuple_find(std::tuple<pack...> t)
     {
         constexpr auto x = [&]<std::size_t... indexes>(std::index_sequence<indexes...>)
         {
-            return (value_is_value<indexes, std::get<indexes>(t), value_to_find>{}() + ...);
+            return (mlib::value_is_value<indexes, std::get<indexes>(t), value_to_find>{}() + ...);
         }(std::make_index_sequence<sizeof...(pack)>{});
         if constexpr (x == 0 && std::get<0>(t) != value_to_find)
         {
